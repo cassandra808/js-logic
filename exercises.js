b1ab1c9135a9a20c82db14fa73b2547ee7eaf5a9
Cassandra Loh <cassandra@Cassandras-MacBook-Air.local>
2018-04-16 16:10:19 -1000

    barely completed final boss

--- exercises.js
+++ exercises.js
@@ -320,6 +320,7 @@ console.log ("bought", doughnutBought)
 /*Create a function name dailySpecials which takes in a parameter: `special`.
 Inside the function, create a switch statement that will check the daily specials of your favorite restaurant (or make up your own daily specials for each day of the week.*/
 
+
 function dailySpecials (special){
   switch (special){
     case 'Monday':
@@ -337,10 +338,13 @@ function dailySpecials (special){
   case 'Friday' :
     menu = 'poke bowl';
     break;
+  default:
+    menu = 'NO SPECIAL';
 }
 return menu;
 }
 console.log (dailySpecials('Friday'));
+
 
 /*
 For loops - A for loop checks a condition a specific number of times and allows us to execute a code block and evaluate a condition to determine if our loop should run again.
@@ -368,7 +372,9 @@ for (var i = 0; i<toyotaModels.length; i++){
  * "Player: 4"
  * "Player: 5"
 */
-
+for (var i = 0; i < 6; i++){
+  console.log ("Player: " + [i])
+}
 
 /* 
  * #16
@@ -376,6 +382,9 @@ for (var i = 0; i<toyotaModels.length; i++){
 */
   var myFavFoods = ["lemon bar", "carrot cake", "nachos", "bacon cheeseburger", "ramen", "sweet potato fries", "chimichanga"];
 
+  for (var i = 0; i < myFavFoods.length; i++){
+    console.log (myFavFoods[i])
+  }
 
 /*
  * #17
@@ -391,8 +400,18 @@ for (var i = 0; i<toyotaModels.length; i++){
  * The function will loop through and add up all the values in the array that is passed into the function and return the total.
  * Console.log your result.
 */
+var numArray = [2, 14, 66, 939, 84]
 
 
+function sumItUp (arr){
+  var total = 0
+  for (var i = 0; i < arr.length; i++){
+    console.log (arr[i]) 
+    total += arr[i]
+  }
+  return total
+}
+console.log (sumItUp(numArray))
 
 /*
  * #18
@@ -405,8 +424,24 @@ for (var i = 0; i<toyotaModels.length; i++){
  * The function will loop through the players array and will put all the even number indexed players in the `east` array and the rest in the `west` array.
  * Console.log both the east and west arrays.
 */ 
-
 var players = ["Yao Ming", "BrookLin", " Jesus Shuttlesworth", "Manute Bol", "Sidney Deane", "World B Free"];
+var east = []
+var west = []
+
+function allStars (ballers){
+  for (var i = 0; i < ballers.length; i++){
+  console.log (ballers[i])
+  if (i%2 === 0){
+    east.push(ballers[i])
+  }else{
+    west.push(ballers[i])
+    }
+  }
+}
+allStars (players)
+console.log("east: ", east);
+console.log("west: ", west);
+
 /*
  * #19
  * Function - subways
@@ -420,7 +455,17 @@ var players = ["Yao Ming", "BrookLin", " Jesus Shuttlesworth", "Manute Bol", "Si
 */ 
 
   var subOftheDay = ["Teriyaki Chicken", "Spicy Italian", "Turkey", "BMT", "Black Forest Ham", "Meatball Marinara", "Veggie"];
-
+function subways (special){
+  for (var i =0; i < special.length; i++){
+    if (i%2 === 1){
+      console.log (i)
+      subOftheDay.splice(i, 1, "Classic Tuna")
+    }
+  }
+  return special
+}
+subways (subOftheDay)
+console.log (subOftheDay)
 
 /*
 Final Boss
@@ -435,16 +480,18 @@ Final Boss
 */
 
 var phrase = "An apple a day keeps Alice feeling awesome!";
+var singleLetters = []
  
+function removeLetter (str){
+  for (var i = 0; i < str.length; i++){
+    if (str[i] !== "a" && str[i] !== "A")
+    singleLetters.push(str[i])
+  }
+  return singleLetters
+}
   
-  
+removeLetter(phrase)
+console.log (removeLetter(phrase))
 
 
-
-
-
-
-
-
-
-
+    
